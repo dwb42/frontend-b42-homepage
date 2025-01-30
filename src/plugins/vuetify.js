65e01a397113a5aacacc3de1d35572bcccd3c2ae
DietrichWedegar <25145840-DietrichWedegar@users.noreply.replit.com>
2025-01-30 09:52:25 +0000

    User checkpoint: Update Vuetify theme colors to teal.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -12,13 +12,16 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+// https://vuetifyjs.com/en/styles/colors/#material-colors
 export default createVuetify({
   theme: {
     themes: {
       light: {
         colors: {
-          primary: '#1867C0',   // Change this hex color
-          secondary: '#5CBBF6', // Change this hex color
+          //primary: '#1867C0',   // indigo
+          primary: '#00695C',   // teal 
+          //secondary: '#5CBBF6', 
+          secondary: '#26A69A', 
         },
       },
     },
